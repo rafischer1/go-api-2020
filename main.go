6cb0b22dd124c3b98c85516a5a4a08769139facf
Artie Fischer <artie@curetechnologies.us>
2020-06-21 21:16:04 -0600

    refactor(main): error logging to fmt and alias of handlers and data

--- main.go
+++ main.go
@@ -4,9 +4,8 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"workspace/portfolio-go-api-2020/data"
-	"workspace/portfolio-go-api-2020/handlers"
-	"workspace/portfolio-go-api-2020/helpers"
+	d "workspace/portfolio-go-api-2020/data"
+	h "workspace/portfolio-go-api-2020/handlers"
 )
 
 func main() {
@@ -14,20 +13,20 @@ func main() {
 	r.LoadHTMLFiles("templates/index.html", "templates/resume.html")
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title": data.WelcomeMessage,
+			"title": d.WelcomeMessage,
 		})
 	})
 	r.GET("/resume", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "resume.html", gin.H{})
 	})
 	r.GET("/plans", func(c *gin.Context) {
-		handlers.GetAll("plans", c)
+		h.GetAll("plans", c)
 	})
 	r.GET("/companies", func(c *gin.Context) {
-		handlers.GetAll("companies", c)
+		h.GetAll("companies", c)
 	})
 	r.GET("/contact", func(c *gin.Context) {
-		handlers.GetAll("contact", c)
+		h.GetAll("contact", c)
 	})
 	err := r.Run() // listen and serve on :8080
 	if err != nil {

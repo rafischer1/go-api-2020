67c21ef1393b9277db5fbfd54687d93601f33b81
Artie Fischer <artie@curetechnologies.us>
2020-07-13 12:21:19 -0600

    feat(email): pdf download to root folder
    to do: create response to send download back to browser and initiate download

--- handlers/email.go
+++ handlers/email.go
@@ -2,9 +2,17 @@ package handlers
 
 import (
 	"fmt"
+	"github.com/cavaliercoder/grab"
+	"log"
 )
 
 // Email (params: address string from Url) start email service
 func Email(address string) {
 	fmt.Println("in the email service!:", address)
+	resp, err := grab.Get(".", "http://www.golang-book.com/public/pdf/gobook.pdf")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Download saved to", resp.Filename)
 }

f393a5c91da91e4eb4782583c5e42615b6ae13a2
Artie Fischer <artie@curetechnologies.us>
2022-01-30 13:02:54 -0700

    wip: adds mock data and removes logs

--- data/trek_books.go
+++ data/trek_books.go
@@ -4,6 +4,7 @@ package data
 type TrekBook struct {
 	ID         int            `json:"id"`
 	Name       string         `json:"name"`
+	Series     string         `json:"series"`
 	Year       int            `json:"year"`
 	BookReview TrekBookReview `json:"book_review"`
 }
@@ -16,4 +17,4 @@ type TrekBookReview struct {
 }
 
 // TrekBooks array
-var TrekBooks = []TrekBook{{ID: 1, Name: "Spock's World", Year: 1993, BookReview: TrekBookReview{TrekBookID: 1, Author: "RAF", Review: "Awesome."}}, {ID: 2, Name: "Trek To Madworld", Year: 1979, BookReview: TrekBookReview{TrekBookID: 2, Author: "RAF", Review: "Meh."}}}
+var TrekBooks = []TrekBook{{ID: 1, Name: "Spock's World", Year: 1993, Series: "TOS", BookReview: TrekBookReview{TrekBookID: 1, Author: "RAF", Review: "Awesome."}}, {ID: 2, Name: "Trek To Madworld", Year: 1979, Series: "TOS", BookReview: TrekBookReview{TrekBookID: 2, Author: "RAF", Review: "Meh."}}, {ID: 3, Name: "Gulliver's Fugitives", Year: 2001, Series: "TNG", BookReview: TrekBookReview{TrekBookID: 3, Author: "RAF", Review: "Yeah."}}, {ID: 4, Name: "Millennium", Year: 2003, Series: "DS9"}}
--- handlers/switch.go
+++ handlers/switch.go
@@ -5,15 +5,11 @@ import (
 	"workspace/portfolio-go-api-2020/data"
 
 	"github.com/gin-gonic/gin"
-	log "github.com/sirupsen/logrus"
 )
 
 // GetAll switch statement for all static data handlers params:(path string, context, params?)
 func GetAll(path string, c *gin.Context, params string) {
 	c.Header("Access-Control-Allow-Origin", "*")
-	log.WithFields(log.Fields{
-		"[Path]": "/" + path,
-	}).Info("params: " + params)
 	switch path {
 	case "plans":
 		c.JSON(200, data.Plans)
@@ -35,9 +31,6 @@ func GetByID(path string, c *gin.Context, params string) {
 	c.Header("Access-Control-Allow-Origin", "*")
 	switch path {
 	case "trek/:id":
-		log.WithFields(log.Fields{
-			"[Path] TRek Path": "/" + path,
-		}).Info("params: " + params)
 		res := []data.TrekBook{}
 		for i := range data.TrekBooks {
 			if fmt.Sprint(data.TrekBooks[i].ID) == params {
